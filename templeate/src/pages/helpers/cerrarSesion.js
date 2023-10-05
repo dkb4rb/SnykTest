ba0989afe68473763ece2c407368244c4e8fd96a
dkb4rb <jjuannca651@gmail.com>
2023-10-05 14:06:43 -0500

    test

--- templeate/src/pages/helpers/cerrarSesion.js
+++
@@ -1,8 +0,0 @@
-import Cookies from "universal-cookie";
-
-const cookies = new Cookies();
-
-export function cerrarSesion() {
-    cookies.remove('id_usr_tok', { path: "/" });
-    window.location.href = '/';
-}
